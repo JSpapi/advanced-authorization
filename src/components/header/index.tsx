7139a1ff73c73afacbfc0e4f62b9ff3383512f48
JSpapi <testing@gmail.com>
2023-08-09 22:11:01 +0500

    finished: signin logic and profile avatar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { AdminPanelSettings, Login, Person } from "@mui/icons-material";
-import { Button, Typography } from "@mui/material";
+import { Avatar, Button, Typography } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
+import { useUser } from "../../hooks/useUsers";
 import s from "./index.module.css";
 
 interface LocationParams {
@@ -14,6 +15,7 @@ interface LocationParams {
 export const Header = () => {
   const loc: LocationParams = useLocation();
 
+  const { user } = useUser();
   return (
     <header className={s.header}>
       <Link to="/">
@@ -21,21 +23,36 @@ export const Header = () => {
           <Typography className="regular">Advanced Auth</Typography>
         </Button>
       </Link>
-
-      {loc.pathname === "/login" && (
-        <Link to="/register" state="Зарегестрироваться">
-          <Button startIcon={<Login />} color="inherit">
-            <Typography className="regular">Зарегестрироваться</Typography>
-          </Button>
+      {user ? (
+        <Link to="/profile" state="Зарегестрироваться">
+          <Avatar
+            alt="User"
+            src={user.profile || ""}
+            sx={{
+              backgroundImage:
+                "linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)",
+              width: 35,
+              height: 35,
+            }}
+          />
         </Link>
-      )}
-
-      {(loc.pathname === "/register" || loc.pathname === "/") && (
-        <Link to="/login" state="Войти">
-          <Button startIcon={<Person />} color="inherit">
-            <Typography className="regular">Войти</Typography>
-          </Button>
-        </Link>
+      ) : (
+        <div>
+          {loc.pathname === "/login" && (
+            <Link to="/register" state="Зарегестрироваться">
+              <Button startIcon={<Login />} color="inherit">
+                <Typography className="regular">Зарегестрироваться</Typography>
+              </Button>
+            </Link>
+          )}
+          {(loc.pathname === "/register" || loc.pathname === "/") && (
+            <Link to="/login" state="Войти">
+              <Button startIcon={<Person />} color="inherit">
+                <Typography className="regular">Войти</Typography>
+              </Button>
+            </Link>
+          )}
+        </div>
       )}
     </header>
   );
