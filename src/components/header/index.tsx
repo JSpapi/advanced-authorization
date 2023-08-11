349a2d9c9be88be85ae518ec34c26e2a66ca0c13
JSpapi <testing@gmail.com>
2023-08-11 22:44:33 +0500

    finished: protecting private routes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,13 @@
-import { AdminPanelSettings, Login, Person } from "@mui/icons-material";
+import {
+  AdminPanelSettings,
+  Login,
+  Person,
+  Security,
+} from "@mui/icons-material";
 import { Avatar, Button, Typography } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import { useUser } from "../../hooks/useUsers";
-import s from "./index.module.css";
+import s from "./index.module.scss";
 
 interface LocationParams {
   pathname: string;
@@ -17,35 +22,42 @@ export const Header = () => {
 
   const { user } = useUser();
   return (
-    <header className={s.header}>
+    <nav className={s.nav}>
       <Link to="/">
-        <Button startIcon={<AdminPanelSettings />} color="inherit">
+        <Button startIcon={<Security />} color="inherit">
           <Typography className="regular">Advanced Auth</Typography>
         </Button>
       </Link>
       {user ? (
-        <Link to="/profile" state="Зарегестрироваться">
-          <Avatar
-            alt="User"
-            src={user.profile || ""}
-            sx={{
-              backgroundImage:
-                "linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)",
-              width: 35,
-              height: 35,
-            }}
-          />
-        </Link>
+        <div className={s.nav_left}>
+          <Link to="/admin">
+            <Button startIcon={<AdminPanelSettings />} color="inherit">
+              <Typography className="regular">Админка</Typography>
+            </Button>
+          </Link>
+          <Link to="/profile">
+            <Avatar
+              alt="User"
+              src={user.profile || ""}
+              sx={{
+                backgroundImage:
+                  "linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)",
+                width: 35,
+                height: 35,
+              }}
+            />
+          </Link>
+        </div>
       ) : (
         <div>
-          {loc.pathname === "/login" && (
+          {(loc.pathname === "/login" || loc.pathname === "/") && (
             <Link to="/register" state="Зарегестрироваться">
               <Button startIcon={<Login />} color="inherit">
                 <Typography className="regular">Зарегестрироваться</Typography>
               </Button>
             </Link>
           )}
-          {(loc.pathname === "/register" || loc.pathname === "/") && (
+          {loc.pathname === "/register" && (
             <Link to="/login" state="Войти">
               <Button startIcon={<Person />} color="inherit">
                 <Typography className="regular">Войти</Typography>
@@ -54,6 +66,6 @@ export const Header = () => {
           )}
         </div>
       )}
-    </header>
+    </nav>
   );
 };
--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,6 +1,6 @@
 import { useTypedSelectors } from "./useTypedSelectors";
 
 export const useUser = () => {
-  const { user } = useTypedSelectors((state) => state.auth);
-  return { user };
+  const { user, isAuthenticated } = useTypedSelectors((state) => state.auth);
+  return { user, isAuthenticated };
 };
