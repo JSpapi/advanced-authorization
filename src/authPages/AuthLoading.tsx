b4a2d45f5c2738db903387d1d52c11aba73e22e1
JSpapi <testing@gmail.com>
2023-08-21 22:27:43 +0500

    refactor: add preloader component to authLoading component

--- src/authPages/AuthLoading.tsx
+++ src/authPages/AuthLoading.tsx
@@ -1,21 +1,11 @@
 import loadingImg from "../assets/preloader.svg";
+import { Preloader } from "../components/preloader/Preloader";
 import { useCurrentQuery } from "../services/auth.api";
 export const AuthLoading = ({ children }: { children: JSX.Element }) => {
   const { isLoading } = useCurrentQuery();
 
   if (isLoading) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <img src={loadingImg} alt="loading" />
-      </div>
-    );
+    return <Preloader image={loadingImg} />;
   }
 
   return children;
