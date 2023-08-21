94bf8d2152feeff7f6884e3f2bb4498e18681730
JSpapi <testing@gmail.com>
2023-08-22 02:53:36 +0500

    feat: sign out logic in profile component

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,26 @@
 import React from "react";
-
+import { Typography } from "@mui/material";
+import s from "./Profile.module.scss";
+import { useAction } from "../../hooks/useActions";
+import { useNavigate } from "react-router-dom";
 export const Profile = () => {
-  return <h1>Profile</h1>;
+  const { AuthLogout } = useAction();
+  const navigate = useNavigate();
+
+  const signOutHandle = () => {
+    AuthLogout();
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  return (
+    <div>
+      <Typography variant="h6" gutterBottom>
+        Here could be user data in case he/she could have willing to update data
+      </Typography>
+
+      <button className={s.signOut} onClick={signOutHandle}>
+        Sign out
+      </button>
+    </div>
+  );
 };
