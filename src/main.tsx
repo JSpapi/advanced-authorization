375d427bbdc7d02d730474d62034b401cb28d10a
JSpapi <testing@gmail.com>
2023-09-12 01:27:15 +0500

    chore: update tsconfig/node.json/ eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.scss";
 import { store } from "./store";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Admin = () => {
   return <h1>Admin</h1>;
 };
